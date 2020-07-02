ff2a60f604f828a76b0ed6601d7be15d68117979
Gram Liu <gram.liu226@gmail.com>
2020-07-02 01:28:33 -0400

    Created team create page

--- src/App.tsx
+++ src/App.tsx
@@ -19,6 +19,7 @@ function App() {
         </Route>
         <Route path="/team">
           <Team />
+        </Route>
         <Route path="/login">
           <Login />
         </Route>
--- src/pages/team/Create.tsx
+++ src/pages/team/Create.tsx
@@ -1,51 +1,74 @@
-import React, { Component, useState } from "react";
-import { Icon, Header, Form, Segment, Grid, Button } from "semantic-ui-react";
+import React from "react";
+import {
+  Form,
+  Segment,
+  Grid,
+  Button,
+  Message,
+  Divider,
+  Image,
+} from "semantic-ui-react";
+import { Link } from "react-router-dom";
+import * as Yup from "yup";
+import { Formik } from "formik";
+import logo from "../../assets/signin-logo.png";
+
+const TeamNameSchema = Yup.object().shape({
+  team_name: Yup.string()
+    .trim()
+    .matches(
+      /[A-Za-z0-9\_\-\ ]+/,
+      "Must be alphanumeric and may have spaces, hyphens, or underscores"
+    )
+    .required("Required")
+});
+
+const TeamCreateForm = () => (
+  <Formik
+    initialValues={{ team_name: "" }}
+    validationSchema={TeamNameSchema}
+    onSubmit={(values: any) => {
+      alert(JSON.stringify(values, null, 2));
+    }}
+  >
+    {(props) => (
+      <Form size="large" onSubmit={props.handleSubmit}>
+        <Segment inverted padded className="black">
+          <Image src={logo} fluid />
+          <Divider hidden />
+          <Form.Input
+            fluid
+            icon="users"
+            iconPosition="left"
+            name="team_name"
+            placeholder="Team Name"
+            onChange={props.handleChange}
+            value={props.values.team_name}
+            error={
+              props.errors.team_name && props.touched.team_name
+                ? props.errors.team_name
+                : null
+            }
+          />
+          <Button type="submit" color="red" fluid size="medium">
+            Create Team
+          </Button>
+        </Segment>
+      </Form>
+    )}
+  </Formik>
+);
 
 const Create = () => {
-  const [teamName, setTeamName] = useState("");
-
-  const handleTeamNameChange = (e:any) => {
-    setTeamName(e.target.value);
-  }
-
   return (
-    <>
-      <Grid
-        textAlign="center"
-        style={{ height: "100vh" }}
-        verticalAlign="middle"
-        centered
-        columns={1}
-      >
-        <Grid.Column style={{ maxWidth: 450 }}>
-          <Header as="h1" textAlign="center">
-            Create/Join Team
-          </Header>
-          <Form size="large">
-            <Form.Input
-              fluid
-              icon="users"
-              iconPosition="left"
-              placeholder="Team name"
-              value={teamName}
-              onChange={handleTeamNameChange}
-            />
-            <Grid columns={2} stackable>
-              <Grid.Column>
-                <Button fluid size="medium">
-                  Create
-                </Button>
-              </Grid.Column>
-              <Grid.Column>
-                <Button fluid size="medium">
-                  Join
-                </Button>
-              </Grid.Column>
-            </Grid>
-          </Form>
-        </Grid.Column>
-      </Grid>
-    </>
+    <Grid textAlign="center" style={{ height: "100vh" }} verticalAlign="middle">
+      <Grid.Column style={{ maxWidth: 350 }}>
+        <TeamCreateForm />
+        <Message color="black">
+          Have a team already? <Link to="/team/join">Join Team</Link>
+        </Message>
+      </Grid.Column>
+    </Grid>
   );
 };
 
