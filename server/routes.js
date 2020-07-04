a0fdabc9a03c70a007f5e0b4d6a0ea17ca53c3cc
Gram Liu <gram.liu226@gmail.com>
2020-07-04 17:53:22 -0400

    Redux bugfix

--- server/routes.js
+++ server/routes.js
@@ -4,12 +4,12 @@ module.exports = function(app) {
 
   // Application ------------------------------------------
   app.get('/', function(req, res){
-    res.sendFile('./client/index.html');
+    res.status(400).send("Forbidden");
   });
 
   // Wildcard all other GET requests to the angular app
   app.get('*', function(req, res){
-    res.sendFile('./client/index.html');
+    res.status(400).send("Forbidden");
   });
 
 };
