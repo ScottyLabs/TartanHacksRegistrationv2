21355218b1500d8272c2f036ad71ba9b10d72d06
Gram Liu <gram.liu226@gmail.com>
2020-07-05 02:52:47 -0400

    Team backend bugfix

--- server/controllers/team/getInvites.js
+++ server/controllers/team/getInvites.js
@@ -3,13 +3,17 @@ const User = require("../../models/User");
 module.exports = async (req, res) => {
   const id = req.params.id;
 
-  const user = await (await User.findById(id)).populate("teamInvitations");
+  const user = await User.findById(id).populate("teamInvitations");
   if (!user) {
     return res.status(404).json({ message: "Unknown user" });
   }
 
+  if (user.teamId) {
+    return res.status(400).json({ message: "User already has a team" });
+  }
+
   const teamInvitations = user.teamInvitations;
-  if (teamInvitations) {
+  if (!teamInvitations || teamInvitations.length == 0) {
     return res.json([]);
   }
   return res.json(teamInvitations);
