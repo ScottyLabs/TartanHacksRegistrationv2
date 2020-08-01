48ffbc8ec436fa00062ce0d303c13750e81bea43
David Ethan Hwang <hdavidethan@gmail.com>
2020-08-01 17:42:46 -0400

    Added dashboard message and sidebar admin checking

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -2,21 +2,44 @@ import React, { useEffect } from "react";
 import SideMenu from "../components/SideMenu";
 import { Grid } from "semantic-ui-react";
 import DashInfo from "../components/DashInfo";
-import { useSelector } from "react-redux";
-import { useDispatch } from "react-redux";
+import { useSelector, useDispatch } from "react-redux";
 import * as actions from "../_actions";
 import { useHistory } from "react-router-dom";
+import { getUser } from "../util/getUserFromState"
+
+interface userStatus {
+  verified: boolean;
+  completed: boolean;
+  admitted: boolean;
+  confirmed: boolean;
+  declined: boolean;
+}
+
+const getStatus = (user: any): userStatus | null => {
+  if (user) {
+    return {
+      verified: user.verified,
+      completed: user.status.completedProfile,
+      admitted: user.status.admitted,
+      confirmed: user.status.confirmed,
+      declined: user.status.declined,
+    };
+  }
+  return null;
+};
 
 const Home = () => {
   const state = useSelector((state) => state);
   const dispatch = useDispatch();
   const history = useHistory();
 
+  const user = getUser(state);
+
   const verify = async () => {
     try {
       const body = {
-        token: window.localStorage.getItem("accessToken")
-      }
+        token: window.localStorage.getItem("accessToken"),
+      };
       await dispatch(actions.users.verifyToken(body));
     } catch {
       history.push("/login");
@@ -26,8 +49,10 @@ const Home = () => {
   useEffect(() => {
     verify();
   }, []);
-  
+
   console.log(state);
+
+  let userStatus = getStatus(user);
 
   return (
     <SideMenu
@@ -35,7 +60,7 @@ const Home = () => {
         <Grid verticalAlign="middle" style={{ height: "100vh" }} centered>
           <Grid.Row>
             <Grid.Column width={10}>
-              <DashInfo name="Andrew Carnegie" verified={false} />
+              <DashInfo name={user?.profile?.name} status={userStatus} />
             </Grid.Column>
           </Grid.Row>
         </Grid>
---
+++ src/util/getUserFromState.ts
@@ -0,0 +1,3 @@
+export const getUser = (state: any): any => {
+  return state?.users?.data?.data?.user;
+}
